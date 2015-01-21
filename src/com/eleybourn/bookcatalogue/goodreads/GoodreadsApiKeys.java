7ea5ff6996d990c647c32227e6b7eccb3da63cb6
Guillaume <guillaume.smaha@gmail.com>
2015-01-21 12:51:11 +0100

    babelio

--- src/com/eleybourn/bookcatalogue/goodreads/GoodreadsApiKeys.java
+++
@@ -1,8 +0,0 @@
-package com.eleybourn.bookcatalogue.goodreads;
-
-public class GoodreadsApiKeys {
-
-	public static final String GOODREADS_DEV_KEY = "LxNilR2de50Ei5m2zPkjQ";
-	public static final String GOODREADS_DEV_SECRET = "xDxJA4OQBcMNCfZpdTUWFcuq6KuJzfaafU7TcAMrfU";
-
-}
