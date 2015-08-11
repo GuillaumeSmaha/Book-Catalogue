148b2209704c0e84298d9609b7f9725120edd9d1
Guillaume Smaha <guillaume.smaha@gmail.com>
2015-08-11 16:35:55 +0200

    Unused file

--- src/com/eleybourn/bookcatalogue/babelio/BabelioApiKeys.java
+++
@@ -1,8 +0,0 @@
-package com.eleybourn.bookcatalogue.babelio;
-
-public class BabelioApiKeys {
-
-	public static final String BABELIO_DEV_KEY = "LxNilR2de50Ei5m2zPkjQ";
-	public static final String BABELIO_DEV_SECRET = "xDxJA4OQBcMNCfZpdTUWFcuq6KuJzfaafU7TcAMrfU";
-
-}
